b197cfca1cbe93245c89e044f3d2a5fd07d035b1
dev <dev>
2022-04-02 12:14:50 +0200

    Kommentare hello.c hinzugefügt.

--- Ch01_GettingStarted/hello.c
+++ Ch01_GettingStarted/hello.c
@@ -1,3 +1,28 @@
+/*
+    Präprozessor Direktiven
+    Die ersten zwei Zeilen bedeuten, dass hier die Präprozessor Direktiven verwendet werden,
+    die sich so verhalten, als ob diese durch die spezifierten Dateien und deren Inhalt an der exakten Stelle
+    ersetzt werden. Wir includieren die <stdio.h> und <stdlib.h> headers um Zugriff auf die 
+    dort deklarierten Funktionen zu bekommen. Die puts Funktion ist deklariert in <stdio.h>
+    und wir können sie nun aus unserem Programm heraus aufrufen. Das Makro EXIT_SUCCESS ist 
+    in <stdlib.h> definiert. <stdio.h> enthält die Deklarationen für C Standard I/O Funktionen.
+    <stdlib.h> enthält die Deklarationen für allgemeine Utility Funktionen. 
+*/
+
+/*
+    Die main Funktion
+    int main(void)
+
+    Diese Zeile definiert die main Funktion die aufgerufen wird beim Start des Programms. Die 
+    main Funktion definiert den Haupteinstiegspunkt für das Programm, welches in einer gehosteten
+    Umgebung ausgeführt wird, und von der Kommando Zeile oder einem anderen Programm aus aufgerufen wird.    
+*/
+
+/*
+    Ausführungsumgebungen
+    C definiert zwei mögliche Ausführungsumgebungen     
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
